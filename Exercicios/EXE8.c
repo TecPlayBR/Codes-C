fd0c4a26f24ed71324af6a44db4b336446c5d262
TecPlayBR <bryan.sharingam@gmail.com>
2026-03-17 11:08:46 -0300

    Atualizado o conteúdo do EXE8.

--- Exercicios/EXE8.c
+++ Exercicios/EXE8.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//FAÇA UM PROGRAMA QUE LEIA DOIS NÚMEROS INTEIROS E IMPRIMA NA TELA SE A SOMA DELES É: MAIOR/IGUAL A 10 OU MENOR QUE 10
+//FAÃ‡A UM PROGRAMA QUE LEIA DOIS Nï¿½MEROS INTEIROS E IMPRIMA NA TELA SE A SOMA DELES FOR MAIOR/IGUAL A 10 OU MENOR QUE 10
 
 int main(){
 
     int numero1;
     int numero2;
 
-    printf("Digite o seu quantas pessoas tem na linha ETINERARIO: ");
+    printf("Digite o seu primeiro numero: ");
     scanf("%d", &numero1);
-    printf("Digite quantas pessoas tem na linha RESFRIAR: ");
+    printf("Digite o seu segundo numero: ");
     scanf("%d", &numero2);
 
     int soma = numero1 + numero2;
 
-    if(soma > 30){
-        printf("LINHA PRODUTIVA");
-    }else if(soma == 10){
-        printf("REFAZER A LINHA E DISTRIBUICAO DE PESSOAS");
+    if(soma > 10){
+        printf("Soma maior ou igual a 10");
     }else{
-        printf("MANDAR O DALTRO EMBORA");
+        printf("Soma menor que 10");
     }
 
     return 0;
